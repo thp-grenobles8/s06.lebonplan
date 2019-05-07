c3501b1288490a7fb9808233535b713c5d4663b8
gui3 <guillaume.3.7.13@gmail.com>
2019-05-07 20:33:57 +0200

    workin on

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,27 @@
 class UsersController < ApplicationController
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(
+      name: params[:name],
+      description: params[:description]
+    )
+      flash[:success] = "Ton progil a été modifié !"
+      redirect_to user_path(@user.id)
+    else
+      flash[:danger] = "Ton profil n'est pas valide !"
+      render :edit
+    end
   end
 end
